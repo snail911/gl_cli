a24061cda61c952a3f80b6f518872faf274b9b9f
snail911 <106596988+snail911@users.noreply.github.com>
2023-09-06 20:32:45 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,11 @@ while getopts ":bt" opt; do
   case $opt in
     b)
       # Команды для сборки OpenSSL
+      make build_openssl
       ;;
     t)
       # Команды для тестирования OpenSSL
+      make test_openssl
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
